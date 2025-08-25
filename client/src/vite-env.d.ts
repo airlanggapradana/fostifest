84354cde6d5bea9ab274d004739fef3a8b36de4c
Airlangga Pradana <ranggavfxartwork@gmail.com>
2025-08-25 18:35:37 +0700

    feat: add ImportMetaEnv interface for environment variable support in Vite

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_BASE_API_URL: string;
+  readonly VITE_BASE_URL: string;
+  // tambahin env lain kalau ada
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
